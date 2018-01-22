a452a746ac4d650dc034e78949dafcb5c5b72301
Unknown <semikendall@gmail.com>
2018-01-22 08:33:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Intro2Script1600
---
+++ try.js
@@ -0,0 +1,9 @@
+/*jslint devel: true, node: true*/
+
+var firstname = prompt('What is your first name?');
+
+var lastname = prompt('What is your last name?');
+
+var favoriteColor = prompt('What is your favorite color?');
+
+alert(firstname + ' ' + lastname + ' ' + favoriteColor);
